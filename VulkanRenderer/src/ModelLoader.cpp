02b3ad914bb8cdd081edcd341db3a987b79e8cde
Ivan Golubev <zxcgi@pm.me>
2022-07-16 22:18:08 +0100

    Using assimp for model loading
    Using assimp for model loading

--- VulkanRenderer/src/ModelLoader.cpp
+++ VulkanRenderer/src/ModelLoader.cpp
@@ -1,4 +1,7 @@
 module;
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 #include <cassert>
 #include <DirectXMath.h>
 #include <filesystem>
@@ -18,6 +21,38 @@ using DirectX::XMFLOAT2;
 namespace
 {
 	using namespace gg;
+
+	void readVertices(aiMesh const* assimpMesh, Mesh& outMesh, unsigned int UVsetNumber)
+	{
+		for (unsigned int faceIndex{ 0 }; faceIndex < assimpMesh->mNumFaces; ++faceIndex)
+		{
+			aiFace* face{ &assimpMesh->mFaces[faceIndex] };
+			for (unsigned int j{ 0 }; j < face->mNumIndices; ++j)
+			{
+				auto vertexIndex = face->mIndices[j];
+				auto assimpVertex = assimpMesh->mVertices[vertexIndex];
+
+				aiVector3D UV = assimpMesh->HasTextureCoords(UVsetNumber)
+					? assimpMesh->mTextureCoords[UVsetNumber][vertexIndex]
+					: aiVector3D {0, 0, 0};
+
+				outMesh.Vertices.emplace_back(
+					static_cast<float>(assimpVertex.x),
+					static_cast<float>(assimpVertex.y),
+					static_cast<float>(assimpVertex.z),
+					1.0f, // w
+					UV.x, UV.y
+				);
+			}
+		}
+	}
+
+	Mesh readMesh(aiMesh const* assimpMesh, aiScene const* scene)
+	{
+		Mesh mesh{};
+		readVertices(assimpMesh, mesh, 0);
+		return mesh;
+	}
 }
 
 namespace gg
@@ -45,7 +80,18 @@ namespace gg
 
 	bool ModelLoader::LoadMeshes(std::string const& modelAbsolutePath, Model& outModel)
 	{
-		return false;
+		Assimp::Importer importer;
+		aiScene const* scene = importer.ReadFile(modelAbsolutePath,
+			  aiProcess_Triangulate
+			| aiProcess_JoinIdenticalVertices
+			| aiProcess_SortByPType
+			| aiProcess_FlipUVs
+		);
+		if (!scene)
+			throw std::runtime_error(std::format("Failed to read the input model: {}, error: {}", modelAbsolutePath, importer.GetErrorString()));
+		for (unsigned int i = 0; i < scene->mNumMeshes; ++i)
+			outModel.meshes.emplace_back(readMesh(scene->mMeshes[i], scene));
+		return true;
 	}
 
 } // namespace gg
